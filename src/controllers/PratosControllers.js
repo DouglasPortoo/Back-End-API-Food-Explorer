2144d0bc8f9fac85733b5fd49f41d7b8f5715f6c
Douglas <portogdouglas@gmail.com>
2023-10-19 11:20:33 -0300

    refatorando req index de pratoscontrollers

--- src/controllers/PratosControllers.js
+++ src/controllers/PratosControllers.js
@@ -2,7 +2,7 @@ const knex = require("../database/knex")
 
 const pratosController = {
   create: async (req, res) => {
-    const { title, description,category ,ingredients} = req.body
+    const { title, description, category, ingredients } = req.body
     const user_id = req.user.id
 
     // const avatarfilename = req.file.filename
@@ -52,47 +52,12 @@ const pratosController = {
   },
 
   index: async (req, res) => {
-    const { title,tags } = req.query
-    const user_id = req.user.id
+    const { title } = req.query
 
-    let pratos
+    console.log(title)
+    let pratos = await knex("pratos").whereLike('title', `%${title}%`)
 
-    if (tags) {
-      const filterTags = tags.split(',').map(tag => tag.trim())
-
-      pratos = await knex("prato_tags")
-        .select([
-          "pratos.id",
-          "pratos.title",
-          "pratos.user_id",
-        ])
-        .where("pratos.user_id", user_id)
-        .whereLike("pratos.title", `%${title}%`)
-        .whereIn("prato_tags.ingredients", filterTags)
-        .innerJoin("pratos", "pratos.id", "prato_tags.prato_id")
-        .groupBy("pratos.id")
-        .orderBy("pratos.title")
-
-    } else {
-      pratos = await knex("pratos")
-      .where({ user_id })
-      .whereLike("title", `%${title}%`)
-      .orderBy("created_at")
-    }
-
-    const userTags = await knex("prato_tags").where({ user_id })
-    console.log(userTags)
-    const pratosWhithTags = pratos.map(prato => {
-      const pratoTags = userTags.filter(ingredients => ingredients.prato_id === prato.id)
-
-      return {
-        ...prato,
-        tags: pratoTags
-      }
-    })
-    console.log(pratosWhithTags)
-
-    return res.json(pratosWhithTags)
+    return res.json(pratos)
   }
 }
 
